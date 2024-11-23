9ac3b596bf298d7dc4377f56e831b84e87e495bf
EWanjie <163054892+EWanjie@users.noreply.github.com>
2024-11-23 19:00:09 +0200

    Active Main Menu

--- Morze/Assets/Script/MainMenuManager.cs
+++ Morze/Assets/Script/MainMenuManager.cs
@@ -2,16 +2,95 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public void PlayGame()
+    [SerializeField] private List<Button> buttonList;
+
+    private bool isPress = false;
+
+    private int shortPressDuration = 55;
+    private int longPressDuration = 100;
+
+    private int currentButton = 0;
+    private int currentShortPress;
+    private int currentPressDuration;
+
+    private void Awake()
     {
-        SceneManager.LoadScene(1);
+        currentShortPress = shortPressDuration;
+        SelectButton();
+    }
+    
+    private void Update()
+    {
+        if (isPress)
+        {
+            if (currentShortPress >= 0)
+                currentShortPress--;
+               
+            if (currentShortPress == 0)
+                SelectButton();
+
+            currentPressDuration--;
+            if (currentPressDuration == 0)
+            {
+                ChangeScene(currentButton);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isPress = true;
+
+            currentShortPress = shortPressDuration;
+            currentPressDuration = longPressDuration;
+
+            SelectButton();            
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isPress = false;
+
+            if(currentShortPress > 0)
+                currentButton = (currentButton + 1) % buttonList.Count;
+
+            SelectButton();
+        }
     }
 
-    public void QuitGame()
+    private void SelectButton()
     {
-        Application.Quit();
+        Color activeColor = (currentShortPress == 0 && isPress) ? Color.yellow : Color.red;
+
+        for (int i = 0; i < buttonList.Count; i++)
+        {
+            Color colorButton = (i == currentButton) ? activeColor : Color.blue;
+
+            var button = buttonList[i];
+            var colors = button.colors;
+            var disabledColor = colors.disabledColor;
+            disabledColor = colorButton;
+            colors.disabledColor = disabledColor;
+            button.colors = colors;
+        }        
+    }
+
+    private void ChangeScene(int button)
+    {
+        switch (button)
+        {
+            case 0:
+                SceneManager.LoadScene(1);
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                Application.Quit();
+                break;
+        }
     }
 }
